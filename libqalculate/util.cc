chengsun/OLD-libqalculate

Don't use hardcoded absolute package data paths on Windows
On Windows, qalculate will look for the data files relative to the
directory of the executable (making PACKAGE_DATA_DIR effectively
"../share" rather than a compile-time determined constant, and similarly
for PACKAGE_LOCALE_DIR)

libqalculate/util.cc
@@ -801,6 +801,35 @@ string getLocalDir() {
 #endif
 }
 
+string getDataDir() {
+#ifdef USE_ABSOLUTE_PACKAGE_PATHS
+	string datadir(PACKAGE_DATA_DIR);
+	return datadir;
+#else
+	char exepath[MAX_PATH];
+	GetModuleFileName(NULL, exepath, MAX_PATH);
+	string datadir(exepath);
+	datadir.resize(datadir.find_last_of('\\'));
+	if (datadir.substr(datadir.length() - 3) == "bin") {
+		datadir.resize(datadir.find_last_of('\\'));
+	}
+	datadir += "\\share";
+	return datadir;
+#endif
+}
+
+string getPackageLocaleDir() {
+#ifdef USE_ABSOLUTE_PACKAGE_PATHS
+	string localedir(PACKAGE_LOCALE_DIR);
+	return localedir;
+#else
+	gchar *dir = g_build_filename(getDataDir().c_str(), "locale", NULL);
+	string localeDir(dir);
+	g_free(dir);
+	return localeDir;
+#endif
+}
+
 
 #ifdef __unix__
 

libqalculate/util.h
@@ -97,6 +97,8 @@ bool equalsIgnoreCase(const string &str1, const char *str2);
 void parse_qalculate_version(string qalculate_version, int *qalculate_version_numbers);
 
 string getLocalDir();
+string getDataDir();
+string getPackageLocaleDir();
 
 class Thread {
 public:

Qalculate/libqalculate

libqalculate/util.cc
@@ -817,6 +817,35 @@ bool move_file(const char *from_file, const char *to_file) {
 	
 }
 
+string getPackageDataDir() {
+#ifdef USE_ABSOLUTE_PACKAGE_PATHS
+	string datadir(PACKAGE_DATA_DIR);
+	return datadir;
+#else
+	char exepath[MAX_PATH];
+	GetModuleFileName(NULL, exepath, MAX_PATH);
+	string datadir(exepath);
+	datadir.resize(datadir.find_last_of('\\'));
+	if (datadir.substr(datadir.length() - 3) == "bin") {
+		datadir.resize(datadir.find_last_of('\\'));
+	}
+	datadir += "\\share";
+	return datadir;
+#endif
+}
+
+string getPackageLocaleDir() {
+#ifdef USE_ABSOLUTE_PACKAGE_PATHS
+	string localedir(PACKAGE_LOCALE_DIR);
+	return localedir;
+#else
+	gchar *dir = g_build_filename(getPackageDataDir().c_str(), "locale", NULL);
+	string localeDir(dir);
+	g_free(dir);
+	return localeDir;
+#endif
+}
+
 
 #ifdef __unix__
 

libqalculate/util.h
@@ -99,6 +99,8 @@ void parse_qalculate_version(string qalculate_version, int *qalculate_version_nu
 
 string getOldLocalDir();
 string getLocalDir();
+string getPackageDataDir();
+string getPackageLocaleDir();
 string getLocalDataDir();
 string getLocalTmpDir();
 bool move_file(const char *from_file, const char *to_file);