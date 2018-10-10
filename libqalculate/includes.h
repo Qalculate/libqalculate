0294100a3ea1000aae897dbd12bf41008cc4ed2c
Hanna K <hanna.knutsson@protonmail.com>
2018-10-10 19:00:33 +0200

    Fix 1/x=0 when x may be infinite; Improve trigonometric transformations; Improve comparison tests; Fix factorization segfault

--- libqalculate/includes.h
+++ libqalculate/includes.h
@@ -642,7 +642,7 @@ static const struct EvaluationOptions {
 	/// If functions will be calculated. Default: true
 	bool calculate_functions;
 	/// If comparisons will be evaluated (ex. 5>2 => 1). Default: true
-	bool test_comparisons;
+	int test_comparisons;
 	/// If a varaible will be isolated to the left side in equations/comparisons (ex. x+y=2 => x=2-y). Default: true
 	bool isolate_x;
 	/// If factors (and bases) containing addition will be expanded (ex. z(x+y)=zx+zy). Default: true
