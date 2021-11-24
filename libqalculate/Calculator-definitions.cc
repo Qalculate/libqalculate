c25a8a00e49eecc7683016ea1ebaa7c31f847f83
Hanna K <hanna.knutsson@protonmail.com>
2021-11-24 16:30:10 +0100

    Apply same logic with separate to command, as with "to" at end of expression, when result contains no unit; Only add Ctrl+L shortcut in Windows; Increment version number; Update translations; Update currencies; Update Doxyfile

--- libqalculate/Calculator-definitions.cc
+++ libqalculate/Calculator-definitions.cc
@@ -820,7 +820,7 @@ int Calculator::loadDefinitions(const char* file_name, bool is_user_defs, bool c
 		xmlFreeDoc(doc);
 		return false;
 	}
-	int version_numbers[] = {3, 21, 0};
+	int version_numbers[] = {3, 22, 0};
 	parse_qalculate_version(version, version_numbers);
 
 	bool new_names = version_numbers[0] > 0 || version_numbers[1] > 9 || (version_numbers[1] == 9 && version_numbers[2] >= 4);
