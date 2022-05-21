243a27a392be3dda73abe2058298e0b0de1fec5d
Hanna K <hanna.knutsson@protonmail.com>
2022-05-21 10:57:50 +0200

    Use Unicode subscripts in information output, in qalc; Inrement version number; Update exchange rates; Update ChangeLog

--- libqalculate/Calculator.cc
+++ libqalculate/Calculator.cc
@@ -169,13 +169,13 @@ Calculator::Calculator() {
 	srand(time(NULL));
 
 	exchange_rates_time[0] = 0;
-	exchange_rates_time[1] = (time_t) 457896L * (time_t) 3600;
+	exchange_rates_time[1] = (time_t) 459192L * (time_t) 3600;
 	exchange_rates_time[2] = 0;
-	priv->exchange_rates_time2[0] = (time_t) 457896L * (time_t) 3600;
+	priv->exchange_rates_time2[0] = (time_t) 459192L * (time_t) 3600;
 	exchange_rates_check_time[0] = 0;
-	exchange_rates_check_time[1] = (time_t) 457896L * (time_t) 3600;
+	exchange_rates_check_time[1] = (time_t) 459192L * (time_t) 3600;
 	exchange_rates_check_time[2] = 0;
-	priv->exchange_rates_check_time2[0] = (time_t) 457896L * (time_t) 3600;
+	priv->exchange_rates_check_time2[0] = (time_t) 459192L * (time_t) 3600;
 	b_exchange_rates_warning_enabled = true;
 	b_exchange_rates_used = 0;
 
@@ -408,13 +408,13 @@ Calculator::Calculator(bool ignore_locale) {
 	srand(time(NULL));
 
 	exchange_rates_time[0] = 0;
-	exchange_rates_time[1] = (time_t) 457896L * (time_t) 3600;
+	exchange_rates_time[1] = (time_t) 459192L * (time_t) 3600;
 	exchange_rates_time[2] = 0;
-	priv->exchange_rates_time2[0] = (time_t) 457896L * (time_t) 3600;
+	priv->exchange_rates_time2[0] = (time_t) 459192L * (time_t) 3600;
 	exchange_rates_check_time[0] = 0;
-	exchange_rates_check_time[1] = (time_t) 457896L * (time_t) 3600;
+	exchange_rates_check_time[1] = (time_t) 459192L * (time_t) 3600;
 	exchange_rates_check_time[2] = 0;
-	priv->exchange_rates_check_time2[0] = (time_t) 457896L * (time_t) 3600;
+	priv->exchange_rates_check_time2[0] = (time_t) 459192L * (time_t) 3600;
 	b_exchange_rates_warning_enabled = true;
 	b_exchange_rates_used = 0;
 
@@ -1775,11 +1775,11 @@ void Calculator::addBuiltinFunctions() {
 }
 void Calculator::addBuiltinUnits() {
 	u_euro = addUnit(new Unit(_("Currency"), "EUR", "euros", "euro", "European Euros", false, true, true));
-	u_btc = addUnit(new AliasUnit(_("Currency"), "BTC", "bitcoins", "bitcoin", "Bitcoins", u_euro, "43380.4", 1, "", false, true, true));
+	u_btc = addUnit(new AliasUnit(_("Currency"), "BTC", "bitcoins", "bitcoin", "Bitcoins", u_euro, "27825.81", 1, "", false, true, true));
 	u_btc->setApproximate();
 	u_btc->setPrecision(-2);
 	u_btc->setChanged(false);
-	priv->u_byn = addUnit(new AliasUnit(_("Currency"), "BYN", "", "", "Belarusian Ruble", u_euro, "1/3.5806", 1, "", false, true, true));
+	priv->u_byn = addUnit(new AliasUnit(_("Currency"), "BYN", "", "", "Belarusian Ruble", u_euro, "1/3.5628", 1, "", false, true, true));
 	priv->u_byn->setHidden(true);
 	priv->u_byn->setApproximate();
 	priv->u_byn->setPrecision(-2);
