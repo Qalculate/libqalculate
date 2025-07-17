1d9bcd0b48632389d7f0f4eff28bbe90c23e8297
Hanna K. <hanna.knutsson@protonmail.com>
2025-07-17 09:55:14 +0200

    Fix description (phonewords digits) for base function; Fix missing closing parenthesis in warning; Add support for "to 1/n" as alternative to "to fraction"

--- libqalculate/Calculator-calculate.cc
+++ libqalculate/Calculator-calculate.cc
@@ -1717,7 +1717,7 @@ string Calculator::calculateAndPrint(string str, int msecs, const EvaluationOpti
 
 long int get_fixed_denominator2(const string &str, NumberFractionFormat &nff, bool b_minus, int frac) {
 	long int fden = 0;
-	if((frac > 0 && EQUALS_IGNORECASE_AND_LOCAL(str, "fraction", _("fraction"))) || (frac == 2 && str == "frac")) {
+	if((frac > 0 && (EQUALS_IGNORECASE_AND_LOCAL(str, "fraction", _("fraction")) || str == "1/n" || str == "/n")) || (frac == 2 && str == "frac")) {
 		fden = -1;
 		if(b_minus) nff = FRACTION_FRACTIONAL;
 		else nff = FRACTION_COMBINED;
@@ -3345,7 +3345,7 @@ MathStructure Calculator::calculate(string str, const EvaluationOptions &eo, Mat
 	if(make_to_division) {
 		separateToExpression(str, str2, eo, true);
 		size_t i = str2.find(RIGHT_PARENTHESIS);
-		if(i != string::npos && str2.rfind(LEFT_PARENTHESIS, i) == string::npos) error(false, _("Conversion of only part of an expression is not supported (\"%s\" is converted to \"%s\"."), str.c_str(), str2.c_str(), NULL);
+		if(i != string::npos && str2.rfind(LEFT_PARENTHESIS, i) == string::npos) error(false, _("Conversion of only part of an expression is not supported (\"%s\" is converted to \"%s\")."), str.c_str(), str2.c_str(), NULL);
 	}
 
 	// retrieve expression after " where " (or "/.") and remove "to ..." from expression
